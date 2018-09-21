4c4930132141b7e24fddc777ac3718e24112b139
Hadron Megantara <hadron@macs-macbook-pro.local>
2018-09-21 12:43:08 +0700

    Initial Commit

---
+++ payment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  payment
+//
+//  Created by Hadron Megantara on 21/09/18.
+//  Copyright © 2018 Hadron Megantara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
