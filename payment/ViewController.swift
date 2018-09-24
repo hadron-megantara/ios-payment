9ca07a1d655d80a190d3f0b55f06ab97f815fc9a
Hadron Megantara <hadron@Macs-MacBook-Pro.local>
2018-09-24 15:39:44 +0700

    PIN Template

--- payment/ViewController.swift
+++ payment/ViewController.swift
@@ -9,10 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var btn1: UIButton!
+    @IBOutlet weak var btn2: UIButton!
+    @IBOutlet weak var btn3: UIButton!
+    @IBOutlet weak var btn4: UIButton!
+    @IBOutlet weak var btn5: UIButton!
+    @IBOutlet weak var btn6: UIButton!
+    @IBOutlet weak var btn7: UIButton!
+    @IBOutlet weak var btn8: UIButton!
+    @IBOutlet weak var btn9: UIButton!
+    @IBOutlet weak var btn0: UIButton!
+    @IBOutlet weak var btnBackSpace: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+//
+        btn1.layer.cornerRadius = 0.5 * btn1.bounds.size.width
+//        btn2.layer.cornerRadius = 5
+//        btn3.layer.cornerRadius = 5
+//        btn4.layer.cornerRadius = 5
+//        btn5.layer.cornerRadius = 5
+//        btn6.layer.cornerRadius = 5
+//        btn7.layer.cornerRadius = 5
+//        btn8.layer.cornerRadius = 5
+//        btn9.layer.cornerRadius = 5
+//        btn0.layer.cornerRadius = 5
+//        btnBackSpace.layer.cornerRadius = 5
     }
 
     override func didReceiveMemoryWarning() {
